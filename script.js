293f441ad0638df904fe8f5a3535806eeca4ce95
Irvan Castro <castroirvan@gmail.com>
2023-06-14 14:42:01 +0800

    Create the display for the calculator

---
+++ script.js
@@ -0,0 +1,14 @@
+// I think logic is every time a button is clicked, the number will be added to respective term
+let firstTerm = [];
+let operator;
+let secondTerm = [];
+
+function storeNum(el) {
+    
+};
+
+
+// 
+function operate() {
+
+};
