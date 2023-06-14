9b66e052b35780b25134cecfc37f1da22901c13d
Irvan Castro <castroirvan@gmail.com>
2023-06-14 14:54:32 +0800

    Create and add storeNum function for numbers and dot button

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // I think logic is every time a button is clicked, the number will be added to respective term
-let firstTerm = [];
-let operator;
-let secondTerm = [];
+let firstTerm = "";
+let operator = "";
+let secondTerm = "";
 
 function storeNum(el) {
-    
+    firstTerm += el.innerHTML
+    console.log(el.innerHTML);
+    document.getElementById("currExp").innerHTML = `${firstTerm}`;
 };
 
 
@@ -12,3 +14,4 @@ function storeNum(el) {
 function operate() {
 
 };
+
